e00247ebca53c4886e7b36c7eccc27e3e8d43254
Vidushika0316 <dasanayakasmvt.19@uom.lk>
2022-07-06 10:38:57 +0530

    Role and notification parts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { FiSettings } from "react-icons/fi";
-import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import { Navbar, Footer, Sidebar } from "./components";
+import { Navbar, Footer, Sidebar } from './components';
 import {
   DashBoard,
   Kanban,
@@ -20,26 +20,32 @@ import {
   ListRoles,
   EditRoles,
   Profile,
-} from "./pages";
+} from './pages';
 
-import "./App.css";
+import './App.css';
 
-import { useStateContext } from "./contexts/ContextProvider";
+import ListNotification from './pages/Notifications/ListNotification';
+import ViewNotification from './pages/Notifications/ViewNotification';
+import EditNotification from './pages/Notifications/EditNotification';
+
+import AddRole from './pages/Roles/AddRole';
+
+import { useStateContext } from './contexts/ContextProvider';
 
 const App = () => {
   const { currentMode, activeMenu, currentColor, setThemeSettings } =
     useStateContext();
 
   return (
-    <div className={currentMode === "Light" ? "light" : ""}>
+    <div className={currentMode === 'Light' ? 'light' : ''}>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
-          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
             <TooltipComponent content="Settings" position="Top">
               <button
                 type="button"
                 onClick={() => setThemeSettings(true)}
-                style={{ background: currentColor, borderRadius: "50%" }}
+                style={{ background: currentColor, borderRadius: '50%' }}
                 className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
               >
                 <FiSettings />
@@ -58,8 +64,8 @@ const App = () => {
           <div
             className={
               activeMenu
-                ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
-                : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
             }
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
@@ -85,7 +91,8 @@ const App = () => {
                 {/* roles*/}
                 <Route path="/roles">
                   <Route index element={<ListRoles />} />
-                  <Route path="edit/:roleid" element={<EditRoles />} />
+                  <Route path="edit/:idrole" element={<EditRoles />} />
+                  <Route path="new" element={<AddRole />} />
                 </Route>
 
                 {/*subscriptions*/}
@@ -95,6 +102,19 @@ const App = () => {
                   <Route path=":subid" element={<ViewSubscription />} />
                   <Route path="edit/:subid" element={<EditSubscription />} />
                   <Route path="bin" element={<DeletedSubscriptions />} />
+                </Route>
+
+                {/* Notification */}
+                <Route path="/notifications">
+                  <Route index element={<ListNotification />} />
+                  <Route
+                    path="view/:idRequest"
+                    element={<ViewNotification />}
+                  />
+                  <Route
+                    path="edit/:idRequest"
+                    element={<EditNotification />}
+                  />
                 </Route>
 
                 {/* apps  */}
